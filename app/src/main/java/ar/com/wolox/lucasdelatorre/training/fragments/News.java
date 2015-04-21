878d1cb9d01ae1ab790a366d908e3ace02f44483
Lucas de la Torre <luksdlt92@gmail.com>
2015-04-21 10:52:34 -0300

    -First Board implementation (with Tabs!)

---
+++ app/src/main/java/ar/com/wolox/lucasdelatorre/training/fragments/News.java
@@ -0,0 +1,19 @@
+package ar.com.wolox.lucasdelatorre.training.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ar.com.wolox.lucasdelatorre.training.R;
+
+public class News extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v =inflater.inflate(R.layout.fragment_news,container,false);
+        return v;
+    }
+}
