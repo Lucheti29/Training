72aa8783593ed451efc9f6072e19c83d346b5d9c
Lucas de la Torre <luksdlt92@gmail.com>
2015-04-21 10:52:34 -0300

    -First Board implementation (with Tabs!)

--- app/src/main/java/ar/com/wolox/lucasdelatorre/training/activities/Board.java
+++ app/src/main/java/ar/com/wolox/lucasdelatorre/training/activities/Board.java
@@ -1,18 +1,56 @@
 package ar.com.wolox.lucasdelatorre.training.activities;
 
-import android.app.Activity;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import ar.com.wolox.lucasdelatorre.training.R;
+import ar.com.wolox.lucasdelatorre.training.supportabs.SlidingTabLayout;
+import ar.com.wolox.lucasdelatorre.training.supportabs.ViewPagerAdapter;
 
-public class Board extends Activity {
+
+public class Board extends ActionBarActivity {
+
+    private Toolbar mToolbar;
+    private ViewPager mPager;
+    private ViewPagerAdapter mAdapter;
+    private SlidingTabLayout mTabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
 
-        //TODO: Need activity_board done
-        setContentView(R.layout.activity_login);
+        init();
+        setUi();
+        populate();
     }
 
+    private void init() {
+        CharSequence titles[] = {"Home","Events"};
+        //Number of tabs -> 2
+        mAdapter =  new ViewPagerAdapter(getSupportFragmentManager(), titles, 2);
+    }
+
+    private void setUi() {
+        mToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mTabs = (SlidingTabLayout) findViewById(R.id.tabs);
+    }
+
+    private void populate() {
+        setSupportActionBar(mToolbar);
+        mPager.setAdapter(mAdapter);
+        mTabs.setDistributeEvenly(true);
+
+        mTabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.greenwolox);
+            }
+        });
+
+        mTabs.setViewPager(mPager);
+    }
 }
