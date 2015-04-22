f5c92297eae8f721a5afe625d0d9e7772b292bf9
Lucas de la Torre <luksdlt92@gmail.com>
2015-04-22 15:50:54 -0300

    -News fragment implemented with Wolox convention -ListView partially implemented -Cleanup

--- app/src/main/java/ar/com/wolox/lucasdelatorre/training/supportabs/ViewPagerAdapter.java
+++ app/src/main/java/ar/com/wolox/lucasdelatorre/training/supportabs/ViewPagerAdapter.java
@@ -1,6 +1,7 @@
 package ar.com.wolox.lucasdelatorre.training.supportabs;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
